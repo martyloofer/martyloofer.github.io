2b13b37de8dde6c6e70bbe4d5aad5124bc17785f
Glen Vollmer <glenvollmer@Glens-MacBook-Pro.local>
2020-07-24 00:08:03 -0500

    Updates

---
+++ precache-manifest.f1ba22b31915720d877cb518f66c6b66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f56b0f06d870276ad4a97e33bf6e1a9",
+    "url": "/martyloofer.github.io/index.html"
+  },
+  {
+    "revision": "5ec8505a4b805ea24b82",
+    "url": "/martyloofer.github.io/static/css/main.17f3fd84.chunk.css"
+  },
+  {
+    "revision": "26cb044a0ef41c0d550a",
+    "url": "/martyloofer.github.io/static/js/2.1bbb3065.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/martyloofer.github.io/static/js/2.1bbb3065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ec8505a4b805ea24b82",
+    "url": "/martyloofer.github.io/static/js/main.f43ec490.chunk.js"
+  },
+  {
+    "revision": "5c09a9bcc1575e609e36",
+    "url": "/martyloofer.github.io/static/js/runtime-main.ee958c32.js"
+  },
+  {
+    "revision": "1a2f83027169a8414725a4fc61c18a13",
+    "url": "/martyloofer.github.io/static/media/test.1a2f8302.png"
+  }
+]);
